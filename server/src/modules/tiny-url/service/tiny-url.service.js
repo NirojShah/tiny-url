2796f6f3151bf9f8da8c9467cc1266cd1066d51c
Niraj Shah <nirajshah950@gmail.com>
2026-02-01 13:57:02 +0530

    service functions to create and to generate and redirect.

---
+++ server/src/modules/tiny-url/service/tiny-url.service.js
@@ -0,0 +1,53 @@
+const CustomError = require("../../../utils/CustomError");
+const tinyUrl = require("../model/tiny-url.model");
+const crypto = require("crypto");
+
+const generateHashLInk = (url) => {
+  const hash = crypto.createHash("sha256");
+  hash.update(url);
+  const fullLength = hash.digest("hex");
+  return fullLength.substring(0, 8);
+};
+
+const processCreateUrl = async (url) => {
+  try {
+    const resp = generateHashLInk(url);
+    const createNew = await tinyUrl.create({
+      originalUrl: url,
+      shortUrl: resp,
+    });
+
+    if (createNew) {
+      return {
+        success: true,
+        message: "successfully created new.",
+        shortUrl: createNew.shortUrl,
+      };
+    }
+    throw new CustomError(500, "something went wrong.");
+  } catch (error) {
+    throw new CustomError(500, error.message);
+  }
+};
+
+const processRedirect = async (shortUrl) => {
+  try {
+    const exists = await tinyUrl.findOne({
+      shortUrl: shortUrl,
+    });
+    if (!exists) {
+      throw new CustomError(404, "Url Not Found.");
+    }
+    return {
+      success: true,
+      url: exists.originalUrl,
+    };
+  } catch (err) {
+    throw new CustomError(500, err.message);
+  }
+};
+
+module.exports = {
+  processCreateUrl,
+  processRedirect,
+};
