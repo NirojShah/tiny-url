c836550b85853893e87289a34ed821d4f6461200
Niraj Shah <nirajshah950@gmail.com>
2026-02-01 13:59:13 +0530

    controller functions for the Tiny - Url.

---
+++ server/src/modules/tiny-url/controller/tiny-url.controller.js
@@ -0,0 +1,32 @@
+const asyncErrorHandler = require("../../../utils/asyncErrorHandler");
+const CustomError = require("../../../utils/CustomError");
+const {
+  processRedirect,
+  processCreateUrl,
+} = require("../service/tiny-url.service");
+
+const createUrl = asyncErrorHandler(async (req, res) => {
+  const { url } = req.body;
+  const resp = await processCreateUrl(url);
+  if (resp.success) {
+    return res.status(201).json({
+      success: true,
+      message: resp.message,
+      url: resp.shortUrl,
+    });
+  }
+});
+
+const redirectUrl = asyncErrorHandler(async (req, res) => {
+  const { shortUrl } = req.params;
+  const resp = await processRedirect(shortUrl);
+  if (resp.success) {
+    res.redirect(resp.url);
+  }
+  throw new CustomError(500, "Url not found.");
+});
+
+module.exports = {
+  createUrl,
+  redirectUrl,
+};
